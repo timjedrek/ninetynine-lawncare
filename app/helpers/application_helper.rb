c1b9bbef238d05257e8e32ca8a4cc0ba76fb31e6
Tim Jedrek <tim@timjedrek.com>
2022-09-08 18:52:37 -0500

    move random bg function to application helper for future random image for contact us

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def randomized_background_image
+    images = ["bg-randomhead0", 
+              "bg-randomhead1", 
+              "bg-randomhead2",
+              "bg-randomhead3",
+              "bg-randomhead4"
+            ]
+            
+    images[rand(images.size)]
+  end
 end
