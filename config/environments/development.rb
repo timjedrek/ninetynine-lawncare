d4a56a4ede2830ab6fd0c068365c7a3cab4ff12e
Tim Jedrek <tim@timjedrek.com>
2022-09-08 20:52:59 -0500

    add mailer config in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,16 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: "99lawncare.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "apikey",
+    password: ENV["SENDGRID_API_KEY"]
+  }
 end
