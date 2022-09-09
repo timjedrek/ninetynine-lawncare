28557fecf3a92c0eb63764d1f171e3b6d7e19743
Tim Jedrek <tim@timjedrek.com>
2022-09-08 22:27:39 -0500

    restrict for admin only

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: %i[ show edit update destroy ]
+  before_action :authenticate_admin!, except: [:new, :create, :confirmation]
 
   # GET /messages or /messages.json
   def index
