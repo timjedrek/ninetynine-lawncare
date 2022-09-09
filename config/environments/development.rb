b2ade52c7ce0b8f1d1cdfb9d49a12be8753043e0
Tim Jedrek <tim@timjedrek.com>
2022-09-08 22:10:43 -0500

    more devise setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,8 @@ Rails.application.configure do
   # switched to true
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
