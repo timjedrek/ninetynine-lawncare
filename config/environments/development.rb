9e7fad1974d376a4b6410f0ce76ccd7e2a10a81c
Tim Jedrek <tim@timjedrek.com>
2022-09-08 20:28:08 -0500

    start on email config for messages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # switched to true
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
